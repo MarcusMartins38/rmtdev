8fc6ffc257377c8b8ca694b73a7fbc32820f06b7
unknown <marcusmartins38@gmail.com>
2024-07-09 00:37:34 -0300

    chore: using portal to popover

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,13 +1,15 @@
 import { forwardRef } from "react";
 import { useBookmarkContext } from "../lib/hooks";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 export const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarkContext();
 
-  return (
+  return createPortal(
     <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
